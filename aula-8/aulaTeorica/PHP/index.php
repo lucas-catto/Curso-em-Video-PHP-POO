18e92c0366e5eb4efb68f239944c5c1feafb3f18
Lucas Catto <lucascattosilva2018@Gmail.com>
2023-05-07 11:48:17 -0300

    Fiz o exemplo da aula em UML e PHP no formato .txt, o exercício em PHP será feito na aula prática. Achei legal essa 'Interação' entre as classes. :>

---
+++ aula-8/aulaTeorica/PHP/index.php
@@ -0,0 +1,6 @@
+<?php
+
+    // Aula Teórica 8 – Relacionamento de Agregação
+    // Nessa aula de POO, vamos aprender como realizar um relacionamento de agregação entre classes para gerar objetos ainda mais poderosos.
+
+?>
