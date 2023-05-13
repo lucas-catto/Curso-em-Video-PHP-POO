c75964363efa20dfe9d5f868da88250a377a3a63
Lucas Catto <lucascattosilva2018@Gmail.com>
2023-05-13 17:26:34 -0300

    Fiz as questões em conjunto da aula.

---
+++ aula-9/aulaTeorica/index.php
@@ -0,0 +1,15 @@
+<style>
+    /*Estou colocando esse CSS pois luz clara dói meu olho.*/ 
+    html{
+        background-color: rgb(150,150,150);
+    }
+</style>
+
+<html>
+    <?php
+
+        // Aula Teórica 9 – Exercícios de POO
+        // Nessa aula de POO, vamos fazer alguns exercícios de Programação Orientada a Objeto conceituais que já apareceram em concursos. Coloque em prática tudo aquilo que aprendeu até aqui.
+
+    ?>
+</html>
