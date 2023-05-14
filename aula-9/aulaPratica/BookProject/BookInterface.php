d0452d342bc77c235dd2290646d4afa09e12e7d3
Lucas Catto <lucascattosilva2018@Gmail.com>
2023-05-13 23:19:26 -0300

    Adicionei um parâmetro ao método 'leafThrough' de página 'page'.

--- aula-9/aulaPratica/BookProject/BookInterface.php
+++ aula-9/aulaPratica/BookProject/BookInterface.php
@@ -2,7 +2,7 @@
     interface Publication{
         public function open();
         public function close();
-        public function leafThrough();
+        public function leafThrough($page);
         public function advancePage();
         public function backPage();
     }
