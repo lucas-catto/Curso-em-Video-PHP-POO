29c99315aa09ef28381f99818c977705970a3e30
Lucas Catto <lucascattosilva2018@Gmail.com>
2023-05-13 23:16:42 -0300

    Fiz o construtor da classe 'BookClass.php'.

--- aula-9/aulaPratica/BookProject/BookClass.php
+++ aula-9/aulaPratica/BookProject/BookClass.php
@@ -1,6 +1,7 @@
 <?php
 
     require_once "BookInterface.php";
+    require_once "PersonClass.php";
 
     class Book implements Publication{
         private $title;
@@ -11,6 +12,13 @@
         private $reader;
 
         public function details(){}
+
+        function __construct($title,$author,$totalPages,$reader){
+            $this->title      = $title;
+            $this->author     = $author;
+            $this->totalPages = $totalPages;
+            $this->reader     = $reader;
+        }
 
         // ===================================
         public function open(){}
