b8c6b1a2525d1dfa56db2dfce758ced3e4c145b8
Lucas Catto <lucascattosilva2018@Gmail.com>
2023-05-13 23:37:19 -0300

    Acabei de fazer o código da classe 'BookClass.php', agora irei começar os testes, incrementei algumas verificações além da aula.

--- aula-9/aulaPratica/BookProject/BookClass.php
+++ aula-9/aulaPratica/BookProject/BookClass.php
@@ -21,11 +21,36 @@
         }
 
         // ===================================
-        public function open(){}
-        public function close(){}
-        public function leafThrough(){}
-        public function advancePage(){}
-        public function backPage(){}
+        public function open(){
+            $this->open = true;
+        }
+        public function close(){
+            $this->open = false;
+        }
+        public function leafThrough($page){
+            if($page > $this->totalPages){ // if the total of page '$page' is bigger than the '$this->totalPages' (total of pages)
+                $this->presentPage = 0;
+            }
+            else{
+                $this->presentPage = $page;
+            }
+        }
+        public function advancePage(){
+            if($this->presentPage < $this->totalPages){
+                $this->presentPage ++;
+            }
+            else{
+                echo "This is already the last page!". $this->presentPage;
+            }
+        }
+        public function backPage(){
+            if($this->presentPage > 0){
+                $this->presentPage --;
+            }
+            else{
+                echo "The page is already 0!";
+            }
+        }
 
     }
 ?>
