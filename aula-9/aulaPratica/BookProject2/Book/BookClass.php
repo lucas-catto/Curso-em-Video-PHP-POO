6c72141c588047d78ffe0eded9253ac4b9b76333
Lucas Catto <lucascattosilva2018@Gmail.com>
2023-05-14 01:31:38 -0300

    Fiz os métodos acessóres (Getters (Get) e Setters (Set)) do 'BookClass.php'.

--- aula-9/aulaPratica/BookProject2/Book/BookClass.php
+++ aula-9/aulaPratica/BookProject2/Book/BookClass.php
@@ -13,5 +13,50 @@
         public function details(){
             
         }
+
+        // -------------------------------------
+        public function getTitle(){
+            return $this->title;
+        }
+        public function setTitle($title){
+            $this->title = $title;
+        }
+        // -------------------------------------
+        public function getAuthor(){
+            return $this->author;
+        }
+        public function setAuthor($author){
+            $this->author = $author;
+        }
+        // -------------------------------------
+        public function getTotalPages(){
+            return $this->totalPages;
+        }
+        public function setTotalPages($totalPages){
+            $this->totalPages = $totalPages;
+        }
+        // -------------------------------------
+        public function getPresentPage(){
+            return $this->presentPage;
+        }
+        public function setPresentPage($presentPage){
+            $this->presentPage = $presentPage;
+        }
+        // -------------------------------------
+        public function getOpen(){
+            return $this->open;
+        }
+        public function setOpen($open){
+            $this->open = $open;
+        }
+        // -------------------------------------
+        public function getReader(){
+            return $this->reader;
+        }
+        public function setReader($reader){
+            $this->reader = $reader;
+        }
+        // -------------------------------------
+
     }
 ?>
