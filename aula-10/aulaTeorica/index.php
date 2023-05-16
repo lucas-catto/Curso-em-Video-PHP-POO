4e6155c535c75c9d1c9d1492a03b47899066b0d9
Lucas Catto <lucascattosilva2018@Gmail.com>
2023-05-15 21:11:11 -0300

    Ainda não acabei.

---
+++ aula-10/aulaTeorica/index.php
@@ -0,0 +1,9 @@
+<style>
+    html{ background-color: rgb(100,100,100); }
+</style>
+<html>
+    <?php
+        // Aula Teórica 10 – Herança (Parte 1)
+        // Nessa aula de POO, vamos aprender o que é Herança em Programação Orientada a Objetos.
+        ?>
+</html>
