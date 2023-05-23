2ac7975d3f1715a97cb992c68305cb03714c73ca
Lucas Silva <lucascattosilva2018@gmail.com>
2023-05-22 22:42:05 -0300

    aula-11

---
+++ aula-11/aulaTeorica/index.php
@@ -0,0 +1,9 @@
+<style>
+    html{ background-color: rgb(100,100,100); }
+</style>
+<html>
+    <?php
+        // Aula Teórica 11 – Herança (Parte 2)
+        // Nessa aula de POO, vamos aprender como funcionam os tipos de Herança, que são a Herança de Implementação e Herança para Diferença. Além disso, vamos ver algumas nomenclaturas importantes para a Programação Orientada a Objetos.
+    ?>
+</html>
