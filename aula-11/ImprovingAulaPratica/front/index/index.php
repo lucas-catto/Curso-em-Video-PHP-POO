a8e1ea4245e91dc6503ab113f90d94ec052495ed
Lucas Silva <lucascattosilva2018@gmail.com>
2023-05-28 09:39:18 -0300

    My first commit of today, all that I've done yesterday!

--- aula-11/ImprovingAulaPratica/front/index/index.php
+++ aula-11/ImprovingAulaPratica/front/index/index.php
@@ -4,12 +4,15 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Index</title>
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <div class="container">
-            <form action="../" method="get">
+            <form action="../page/index.html" method="get">
+
+                <h1>Choose one of the options</h1>
+                
                 <div class="elements">
 
                     <select name="type" id="type">
@@ -21,33 +24,10 @@
                         <option value="tecnic"     >Tecnic</option>
                     </select>
     
+                    <input type="submit" id="submit" value="All finished!">
                 </div>
-                <input type="submit" value="All finished!">
 
             </form>
         </div> <!-- container -->
-
-        <?php
-            $type = isset($_GET["type"]) ? $_GET["type"] : "";
-
-            if($type == "teacher"){ 
-                require_once "..back/TeacherClass.php";
-            }
-            elseif ($type == "visitor"){
-                require_once "../back/VisitorClass.php";
-            }
-            elseif ($type == "student"){
-                require_once "../back/StudentsClass/StudentClass.php";
-            }
-            elseif ($type == "scholarship"){
-                require_once "../back/StudentsClass/ScholarshipClass.php";
-            }
-            elseif ($type == "tecnic"){
-                require_once "../back/Students/TecnincClass.php";
-            }
-            else{
-                echo "Something wrong!";
-            }
-        ?>
     </body>
 </html>
