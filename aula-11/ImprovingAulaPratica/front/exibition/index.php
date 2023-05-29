349b56f3caf75763ff8e39557819cd468d061566
Lucas Silva <lucascattosilva2018@gmail.com>
2023-05-29 20:18:57 -0300

    The CSS of 'exibition/index.php' is getting cool!

--- aula-11/ImprovingAulaPratica/front/exibition/index.php
+++ aula-11/ImprovingAulaPratica/front/exibition/index.php
@@ -12,87 +12,111 @@
     </header>
     <body>
         <div class="container"><pre>
-
-            <!-- PHP + HTML -->
-
-            <?php
-                    // All -  ''
-                $name  = isset($_GET["name"])  ? $_GET["name"]  : false;
-                $age   = isset($_GET["age"])   ? $_GET["age"]   : false;
-                $genre = isset($_GET["genre"]) ? $_GET["genre"] : false;
-                // teacher
-                $specialty = isset($_GET["specialty"]) ? $_GET["specialty"] : false;
-                $salary    = isset($_GET["salary"])    ? $_GET["salary"]    : false;
-                // student-student
-                $course        = isset($_GET["course"])        ? $_GET["course"]        : false;
-                $matriculation = isset($_GET["matriculation"]) ? $_GET["matriculation"] : false;
-                // only 'visitor'
-                $name  = isset($_GET["name"])  ? $_GET["name"]  : false;
-                $age   = isset($_GET["age"])   ? $_GET["age"]   : false;
-                $genre = isset($_GET["genre"]) ? $_GET["genre"] : false;
-                // student-scholarship
-                $scholarship      = isset($_GET["scholarship"])      ? $_GET["scholarship"]      : false;
-                $renewscholarship = isset($_GET["renewscholarship"]) ? $_GET["renewscholarship"] : false;
-                // student-tecnic
-                $professionalRegister = isset($_GET["professionalRegister"]) ? $_GET["professionalRegister"] : false; 
+            <div class="border">
 
 
-                if($name && $age && $genre){
-                    /* --------------------------------------------------------------------------- */
-                    if($salary && $specialty){
-                        require_once "../../back/TeacherClass.php";
+                <!-- PHP + HTML -->
 
-                        $teacher = new Teacher;
+                <?php
+                        // All -  ''
+                    $name  = isset($_GET["name"])  ? $_GET["name"]  : false;
+                    $age   = isset($_GET["age"])   ? $_GET["age"]   : false;
+                    $genre = isset($_GET["genre"]) ? $_GET["genre"] : false;
+                    // teacher
+                    $specialty = isset($_GET["specialty"]) ? $_GET["specialty"] : false;
+                    $salary    = isset($_GET["salary"])    ? $_GET["salary"]    : false;
+                    // student-student
+                    $course        = isset($_GET["course"])        ? $_GET["course"]        : false;
+                    $matriculation = isset($_GET["matriculation"]) ? $_GET["matriculation"] : false;
+                    // only 'visitor'
+                    $name  = isset($_GET["name"])  ? $_GET["name"]  : false;
+                    $age   = isset($_GET["age"])   ? $_GET["age"]   : false;
+                    $genre = isset($_GET["genre"]) ? $_GET["genre"] : false;
+                    // student-scholarship
+                    $scholarship      = isset($_GET["scholarship"])      ? $_GET["scholarship"]      : false;
+                    $renewscholarship = isset($_GET["renewscholarship"]) ? $_GET["renewscholarship"] : false;
+                    // student-tecnic
+                    $professionalRegister = isset($_GET["professionalRegister"]) ? $_GET["professionalRegister"] : false; 
 
-                        $teacher->setName($name);
-                        $teacher->setAge($age);
-                        $teacher->setGenre($genre);
-                        $teacher->setSpecialty($specialty);
-                        $teacher->setSalary($salary);
 
-                        print_r($teacher);
-
-                    } // teacher
-                    /* --------------------------------------------------------------------------- */
-                    elseif($course && $matriculation){
-                        require_once "../../back/StudentsClass/StudentClass.php";
+                    // if(($name && $age && $genre && $course && $matriculation) == true){
+                    //     require_once "../../back/StudentsClass/StudentClass.php";
                         
-                        $student_student = new Student;
+                    //     $student_student = new Student;
 
-                        $student_student->setName($name);
-                        $student_student->setAge($age);
-                        $student_student->setGenre($genre);
-                        $student_student->setCourse($course);
-                        $student_student->setMatriculation($matriculation);
+                    //     $student_student->setName($name);
+                    //     $student_student->setAge($age);
+                    //     $student_student->setGenre($genre);
+                    //     $student_student->setCourse($course);
+                    //     $student_student->setMatriculation($matriculation);
 
-                        print_r($student_student);
+                    //     print_r($student_student);
+                    // }
 
-                    } // student-student
-                    /* --------------------------------------------------------------------------- */
-                    elseif($scholarship && $renewscholarship){
-                        require_once "../../back/StudentsClass/";
-                    } // Scholarship
-                    /* --------------------------------------------------------------------------- */
-                    elseif($professionalRegister){
-                        require_once "../../back/StudentsClass/";
-                    } // Tecnic
-                    /* --------------------------------------------------------------------------- */
+
+                    if($name && $age && $genre){
+                        /* --------------------------------------------------------------------------- */
+                        if($salary && $specialty){
+                            require_once "../../back/TeacherClass.php";
+
+                            $teacher = new Teacher;
+
+                            $teacher->setName($name);
+                            $teacher->setAge($age);
+                            $teacher->setGenre($genre);
+                            $teacher->setSpecialty($specialty);
+                            $teacher->setSalary($salary);
+
+                            echo "<p> Name: "      .$teacher->getName()      ."</p>";
+                            echo "<p> Age: "       .$teacher->getAge()       ."</p>";
+                            echo "<p> Genre: "     .$teacher->getGenre()     ."</p>";
+                            echo "<p> Specialty: " .$teacher->getSpecialty() ."</p>";
+                            echo "<p> Salary: "    .$teacher->getSalary()    ."</p>";
+
+                        } // teacher
+                        /* --------------------------------------------------------------------------- */
+                        elseif($course && $matriculation){
+                            require_once "../../back/StudentsClass/StudentClass.php";
+                            
+                            $student_student = new Student;
+
+                            $student_student->setName($name);
+                            $student_student->setAge($age);
+                            $student_student->setGenre($genre);
+                            $student_student->setCourse($course);
+                            $student_student->setMatriculation($matriculation);
+
+                            print_r($student_student);
+
+                        } // student-student
+                        /* --------------------------------------------------------------------------- */
+                        elseif($scholarship && $renewscholarship){
+                            require_once "../../back/StudentsClass/";
+                        } // Scholarship
+                        /* --------------------------------------------------------------------------- */
+                        elseif($professionalRegister){
+                            require_once "../../back/StudentsClass/";
+                        } // Tecnic
+                        /* --------------------------------------------------------------------------- */
+                        else{
+                            require_once "../../back/VisitorClass.php";
+
+                            $visitor = new Visitor;
+
+                            $visitor->setName($name);
+                            $visitor->setAge($age);
+                            $visitor->setGenre($genre);
+
+                            echo "<p> Name: "      .$visitor->getName()      ."</p>";
+                            echo "<p> Age: "       .$visitor->getAge()       ."</p>";
+                            echo "<p> Genre: "     .$visitor->getGenre()     ."</p>";
+
+                        }// visitor
+                        /* --------------------------------------------------------------------------- */
+                    }
                     else{
-                        require_once "../../back/VisitorClass.php";
-
-                        $visitor = new Visitor;
-
-                        $visitor->setName($name);
-                        $visitor->setAge($age);
-                        $visitor->setGenre($genre);
-
-                        print_r($visitor);
-                    }// visitor
-                    /* --------------------------------------------------------------------------- */
-                }
-                else{
-                    echo "<p>Something is wrong!</p>";
-                }
+                        echo "<p>Something is wrong!</p>";
+                    }
 
 
 
@@ -105,38 +129,38 @@
 
 
 
-                /*
-                fazer a verificação se todas as características são veradedas, então é só instanciar o objeto
-                
+                    /*
+                    fazer a verificação se todas as características são veradedas, então é só instanciar o objeto
+                    
 
-                características:
-                name
-                age
-                genre
-
-
-
-                teacher
-                specialty
-                salary
-
-                visitor
-
-                student
-                course
-                matriculation
-
-                scholarship
-                scholarship
-                renewscholarship
-
-                tecnic
-                rpfessional register
+                    características:
+                    name
+                    age
+                    genre
 
 
-                */
-            ?>
 
+                    teacher
+                    specialty
+                    salary
+
+                    visitor
+
+                    student
+                    course
+                    matriculation
+
+                    scholarship
+                    scholarship
+                    renewscholarship
+
+                    tecnic
+                    rpfessional register
+
+
+                    */
+                ?>
+            </div> <!-- border -->
 
 </pre></div>
     </body>
