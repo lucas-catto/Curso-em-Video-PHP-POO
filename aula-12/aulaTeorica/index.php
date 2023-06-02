e4e844020bd36822c295ae48b5892c6101a5e4c7
Lucas Silva <lucascattosilva2018@gmail.com>
2023-06-01 21:35:38 -0300

    class-12

--- aula-12/aulaTeorica/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-    // Aula Teórica 12 – Conceito Polimorfismo (Parte 1)
-    // Nessa aula de POO, vamos aprender como funciona o Polimorfismo em Programação Orientada a Objetos, o terceiro pilar de teoria.
-?>
