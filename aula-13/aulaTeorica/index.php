f480ae3ab40955f7abebd11f127e42bfab32aae0
Lucas Silva <lucascattosilva2018@gmail.com>
2023-06-10 15:04:08 -0300

    aula-13

---
+++ aula-13/aulaTeorica/index.php
@@ -0,0 +1,8 @@
+<style>html{background-color:gray;}</style>
+
+<html>
+    <?php
+        // Aula Teórica 13 – Conceito Polimorfismo (Parte 2)
+        // Nessa aula de POO, vamos aprender como aplicar o Polimorfismo de Sobrecarga às nossas classes. Veja também a diferença entre sobrecarga e sobreposição, algo que muita gente confunde.
+    ?>
+</html>
