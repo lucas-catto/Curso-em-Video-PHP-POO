d0b2ac81ccea35c12e4a478bef79dc44d7d0f0fb
Lucas Silva <lucascattosilva2018@gmail.com>
2023-06-11 16:19:07 -0300

    class-14

---
+++ aula-14/aulaTeorica/index.php
@@ -0,0 +1,9 @@
+
+<style>html{background-color:gray;}</style>
+
+<html>
+    <?php
+        // Aula Teórica 14 – Exercícios de POO (Parte 2)
+        // Nessa aula de POO, vamos fazer uma lista de exercícios de programação orientada a objetos para você testar os seus conhecimentos adquiridos durante as 13 primeiras aulas do curso.
+    ?>
+</html>
